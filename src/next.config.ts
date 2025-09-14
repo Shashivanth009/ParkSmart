a43c0c008d39626d5189a0efd182e2c8cf8dca5b
Shashivanth M <chandhumunnanuru@gmail.com>
2025-09-14 16:32:25 +0000

    Console Error
    Error: Failed to load chunk /_next/static/chunks/%5Bturb

---
+++ src/next.config.ts
@@ -0,0 +1,34 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+  experimental: {
+    allowedDevOrigins: [
+      "https://6000-firebase-studio-1747935835035.cluster-ejd22kqny5htuv5dfowoyipt52.cloudworkstations.dev"
+    ],
+  },
+};
+
+export default nextConfig;
